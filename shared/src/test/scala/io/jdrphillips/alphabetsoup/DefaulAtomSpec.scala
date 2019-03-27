7f83c4b0969d5d7aaa19eef604624d875e54d118
Sean McGroarty <sean.mcgroarty@concentra.co.uk>
2019-03-27 17:45:08 +0000

    fixed spacing issue caused by sublime text

--- shared/src/test/scala/io/jdrphillips/alphabetsoup/DefaulAtomSpec.scala
+++ shared/src/test/scala/io/jdrphillips/alphabetsoup/DefaulAtomSpec.scala
@@ -9,108 +9,108 @@ import shapeless.test.illTyped
 
 class DefaultAtomSpec extends FlatSpec with Matchers {
 
-	"Mixer" should "work with a supplied default" in {
-	   
-	    case class Source(a: Int)
-	    case class Target(a: Int, b: String)
+  "Mixer" should "work with a supplied default" in {
 
-	    implicit val default: Atom.DefaultAtom[String] = Atom.DefaultAtom("default")
+  case class Source(a: Int)
+  case class Target(a: Int, b: String)
 
-	    val mixer = Mixer[Source, Target]
-	    
-	    val source = Source(1)
+  implicit val default: Atom.DefaultAtom[String] = Atom.DefaultAtom("default")
 
-	    mixer.mix(source) shouldBe Target(1, "default")
+  val mixer = Mixer[Source, Target]
 
-	  }
-	  it should "return types from left ignoring default" in {
+  val source = Source(1)
 
-	  	case class Source(a: String)
-	  	case class Target(a: String)
+  mixer.mix(source) shouldBe Target(1, "default")
 
-	  	implicit val default: Atom.DefaultAtom[String] = Atom.DefaultAtom("default")
+  }
+  it should "return types from left ignoring default" in {
 
-	  	val mixer = Mixer[Source, Target]
+    case class Source(a: String)
+    case class Target(a: String)
 
-	  	mixer.mix(Source("source")) shouldBe Target("source")
-	  }
-	  it should "work with multiple supplied defaults" in {
-	   
-	    case class Source(a: Int)
-	    case class Target(a: Int, b: String, c: Boolean, d: Long)
+    implicit val default: Atom.DefaultAtom[String] = Atom.DefaultAtom("default")
 
-	    implicit val defaultString: Atom.DefaultAtom[String] = Atom.DefaultAtom("default")
-	    implicit val defaultBoolean: Atom.DefaultAtom[Boolean] = Atom.DefaultAtom(true)
-	    implicit val defaultLong: Atom.DefaultAtom[Long] = Atom.DefaultAtom(10L)
+    val mixer = Mixer[Source, Target]
 
-	    val mixer = Mixer[Source, Target]
-	    
-	    val source = Source(1)
+    mixer.mix(Source("source")) shouldBe Target("source")
+  }
+  it should "work with multiple supplied defaults" in {
 
-	    mixer.mix(source) shouldBe Target(1, "default", true, 10L)
+    case class Source(a: Int)
+    case class Target(a: Int, b: String, c: Boolean, d: Long)
 
-	  }
-	  it should "not work with molecules containing no internal structure" in {
-	    case class Source(a: Int)
-	    case class Target(a: Int, b: List[String], c: Unit)
+    implicit val defaultString: Atom.DefaultAtom[String] = Atom.DefaultAtom("default")
+    implicit val defaultBoolean: Atom.DefaultAtom[Boolean] = Atom.DefaultAtom(true)
+    implicit val defaultLong: Atom.DefaultAtom[Long] = Atom.DefaultAtom(10L)
 
-	    implicit val default: Atom.DefaultAtom[String] = Atom.DefaultAtom("default")
-	    
-	    illTyped("Mixer[Source, Target]")
+    val mixer = Mixer[Source, Target]
 
-	  }  
-	  
-	  it should "work with molecules" in {
-	    case class Source(a: Int, b: List[Tuple1[Long]])
-	    case class Target(a: Int, b: List[(Long,String)])
+    val source = Source(1)
 
-	    implicit val default: Atom.DefaultAtom[String] = Atom.DefaultAtom("default")
-	    
-	    val mixer = Mixer[Source, Target]
+    mixer.mix(source) shouldBe Target(1, "default", true, 10L)
 
-	    mixer.mix(Source(1, List(Tuple1(10L)))) shouldBe Target(1, List((10L,"default")))
-	  }
-	  
-	  it should "override molecule behaviour with default" in {
-	    case class Source(a: Int)
-	    case class Target(a: Int, b: List[String], c: Unit)
+  }
+  it should "not work with molecules containing no internal structure" in {
+    case class Source(a: Int)
+    case class Target(a: Int, b: List[String], c: Unit)
 
-	    implicit val default: Atom.DefaultAtom[List[String]] = Atom.DefaultAtom(List("default"))
-	    
-	    val mixer = Mixer[Source, Target]
+    implicit val default: Atom.DefaultAtom[String] = Atom.DefaultAtom("default")
 
-	    mixer.mix(Source(1)) shouldBe Target(1, List("default"), ())
-	  }
+    illTyped("Mixer[Source, Target]")
 
-	  it should "work when it finds a HNil before traversing entire structure" in {
+  }
 
-	  	case class Source(a : Int)
+  it should "work with molecules" in {
+    case class Source(a: Int, b: List[Tuple1[Long]])
+    case class Target(a: Int, b: List[(Long,String)])
 
-	  	implicit val default: Atom.DefaultAtom[String] = Atom.DefaultAtom("default")
+    implicit val default: Atom.DefaultAtom[String] = Atom.DefaultAtom("default")
 
-	  	AtomSelector[(Int :: HNil) :: (String :: HNil) :: HNil, String].apply((1 :: HNil) :: ("Me!" :: HNil) :: HNil) shouldBe "Me!"
-	  	
-	  }
+    val mixer = Mixer[Source, Target]
 
-	  it should "always work with HNil in this particular position" in {
+    mixer.mix(Source(1, List(Tuple1(10L)))) shouldBe Target(1, List((10L,"default")))
+  }
 
-	  	implicit val default: Atom.DefaultAtom[String] = Atom.DefaultAtom("default")
+  it should "override molecule behaviour with default" in {
+    case class Source(a: Int)
+    case class Target(a: Int, b: List[String], c: Unit)
 
-	  	AtomSelector[Int :: (String :: HNil) :: HNil, String].apply(1 :: ("Me!" :: HNil) :: HNil) shouldBe "Me!"
-	  	
-	  }
+    implicit val default: Atom.DefaultAtom[List[String]] = Atom.DefaultAtom(List("default"))
 
-	 "MixerBuilder" should "take precedence over supplied defaults" in {
-	 		case class Source(a: Int)
-	 		case class Target(a: Int, b: String)
+    val mixer = Mixer[Source, Target]
 
-	 		implicit val default: Atom.DefaultAtom[String] = Atom.DefaultAtom("Don't use me!!!")
+    mixer.mix(Source(1)) shouldBe Target(1, List("default"), ())
+  }
 
-	 		val mixer = Mixer.from[Source].to[Target].withDefault("I AM THE REAL DEFAULT").build
+  it should "work when it finds a HNil before traversing entire structure" in {
 
-	 		mixer.mix(Source(1)) shouldBe Target(1, "I AM THE REAL DEFAULT")
+    case class Source(a : Int)
+
+    implicit val default: Atom.DefaultAtom[String] = Atom.DefaultAtom("default")
+
+    AtomSelector[(Int :: HNil) :: (String :: HNil) :: HNil, String].apply((1 :: HNil) :: ("Me!" :: HNil) :: HNil) shouldBe "Me!"
+
+  }
+
+  it should "always work with HNil in this particular position" in {
+
+    implicit val default: Atom.DefaultAtom[String] = Atom.DefaultAtom("default")
+
+    AtomSelector[Int :: (String :: HNil) :: HNil, String].apply(1 :: ("Me!" :: HNil) :: HNil) shouldBe "Me!"
+
+  }
+
+  "MixerBuilder" should "take precedence over supplied defaults" in {
+    case class Source(a: Int)
+    case class Target(a: Int, b: String)
+
+    implicit val default: Atom.DefaultAtom[String] = Atom.DefaultAtom("Don't use me!!!")
+
+    val mixer = Mixer.from[Source].to[Target].withDefault("I AM THE REAL DEFAULT").build
+
+    mixer.mix(Source(1)) shouldBe Target(1, "I AM THE REAL DEFAULT")
 
 
-	 }
+  }
 
 }
