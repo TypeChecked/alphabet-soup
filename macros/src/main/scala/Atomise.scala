260521a504ff9d284cfc9735de96d5e49109b173
McGizzle <mcgroas@tcd.ie>
2019-04-11 12:06:13 +0100

    append to companion object if one exists, add unit tests

--- macros/src/main/scala/Atomise.scala
+++ macros/src/main/scala/Atomise.scala
@@ -12,22 +12,35 @@ object AtomicMacro {
   def impl(c: Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
     import c.universe._
 
-    def atomFormatter(className: TypeName) = {
-        q"""object ${className.toTermName} { implicit val ${TermName(className.toTermName.toString + "atom")}: Atom[$className] = Atom[$className] }"""
+    def mkAtomImplicit(className: TypeName) = {
+        q"""implicit val ${TermName(className.toTermName.toString + "atom")}: Atom[$className] = Atom[$className]"""
     }
 
     def extractClassName(classDecl: ClassDef) = {
       try {
-        val q"case class $className(..$fields) extends ..$bases { ..$body }" = classDecl
+        val q"case class $className(..$_) extends ..$_ { ..$_ }" = classDecl
         className
       } catch {
-        case _: MatchError => c.abort(c.enclosingPosition, "Annotation is only supported on case class")
+        case _: MatchError => c.abort(c.enclosingPosition, "@Atmic annotation is only supported on case class")
+      }
+    } 
+
+    def modifiedCompanion(maybeCompDecl: Option[ModuleDef], atomImplicit: ValDef, className: TypeName) = {
+      maybeCompDecl.fold(q"object ${className.toTermName} { $atomImplicit }"){ compDecl =>
+        val q"object $obj extends ..$bases { ..$body }" = compDecl
+        q"""
+          object $obj extends ..$bases {
+            ..$body
+            $atomImplicit
+          }
+        """
       }
     }
 
-    def modifiedClass(classDecl: ClassDef) = {
+    def modifiedDecl(classDecl: ClassDef, maybeCompDecl: Option[ModuleDef] = None) = {
       val className = extractClassName(classDecl)
-      val companion = atomFormatter(className)
+      val atomImplicit = mkAtomImplicit(className)
+      val companion = modifiedCompanion(maybeCompDecl, atomImplicit, className)
 
       c.Expr(q"""
         $classDecl
@@ -36,7 +49,8 @@ object AtomicMacro {
     }
 
     annottees.map(_.tree).toList match {
-        case (classDecl: ClassDef) :: Nil => modifiedClass(classDecl)
+        case (classDecl: ClassDef) :: Nil => modifiedDecl(classDecl)
+        case (classDecl: ClassDef) :: (compDecl: ModuleDef) :: Nil => modifiedDecl(classDecl, Some(compDecl))
         case _ => c.abort(c.enclosingPosition, "Invalid Annotee")
       }
   }
