93fcfa35148b6eff2ea979d78df43d30f5ec4bc0
McGizzle <mcgroas@tcd.ie>
2019-04-11 11:13:07 +0100

    place definition inside companion object

--- shared/src/test/scala/io/typechecked/alphabetsoup/AtomMacroSpec.scala
+++ shared/src/test/scala/io/typechecked/alphabetsoup/AtomMacroSpec.scala
@@ -1,8 +1,16 @@
 package io.typechecked.alphabetsoup
 import macros._
+import org.scalatest._
 
-class AtomMacroSpec {
-  @Atomic case class MakeMeAnAtom()
+class AtomMacroSpec extends FlatSpec with Matchers {
 
-  implicitly[Atom[MakeMeAnAtom]]
+	"@Atomic" should "create implicit Atom for an empty case class" in {
+
+		@Atomic case class MakeMeAnAtom()
+
+  	implicitly[Atom[MakeMeAnAtom]]
+	}
+	// it should "create an implicit Atom for a case class with a single field" in {
+		
+	// }
 }
