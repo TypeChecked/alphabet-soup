5951d7fe0bd0900d6b36350f42fec22436bac91d
McGizzle <mcgroas@tcd.ie>
2019-04-18 12:09:39 +0100

    fix typo and name change in tests

--- alphabet-soup/src/test/scala/io/typechecked/alphabetsoup/MixerReplaceSpec.scala
+++ alphabet-soup/src/test/scala/io/typechecked/alphabetsoup/MixerReplaceSpec.scala
@@ -6,18 +6,18 @@ import shapeless.test.illTyped
 
 class MixerReplaceSpec extends FlatSpec with Matchers {
 
-  "Mixer.injectq" should "work for simple types" in {
+  "Mixer.inject" should "work for simple types" in {
     case class Source(a: Int)
     case class Target(b: Int)
 
-    Mixer[Source, Target].injectq(Target(7), Source(1)) shouldBe Source(7)
+    Mixer[Source, Target].inject(Target(7), Source(1)) shouldBe Source(7)
   }
   it should "work for nested types" in {
     @Atomic case class Inner(i: Int)
     case class Source(i: Inner)
     case class Target(i: Inner)
 
-    Mixer[Source, Target].injectq(Target(Inner(7)), Source(Inner(12))) shouldBe Source(Inner(7))
+    Mixer[Source, Target].inject(Target(Inner(7)), Source(Inner(12))) shouldBe Source(Inner(7))
   }
   it should "work when source is a complex nested structure" in {
     case class MostInner(s: String)
@@ -31,7 +31,7 @@ class MixerReplaceSpec extends FlatSpec with Matchers {
 
     val target = Target(MostInner("Nah"))
 
-    Mixer[Source, Target].injectq(target, source) shouldBe Source(Inner1(MostInner("Nah")), Inner2(MostInner("Nah")))
+    Mixer[Source, Target].inject(target, source) shouldBe Source(Inner1(MostInner("Nah")), Inner2(MostInner("Nah")))
   }
   it should "work for complex structures" in {
     case class A(i: Int, b: Boolean)
@@ -51,19 +51,19 @@ class MixerReplaceSpec extends FlatSpec with Matchers {
     val source = Source(17, "DANGER", List(A(0, false), A(0, true), A(0, false)), List(B("NOOO"), B("NOT MEEEEE")))
     val target = Target(7, BS(List(b1, b2)), List(true -> 1, false -> 2, true -> 3))
 
-    Mixer[Source, Target].injectq(target, source) shouldBe Source(7, "DANGER", List(a1, a2, a3), List(b1, b2))
+    Mixer[Source, Target].inject(target, source) shouldBe Source(7, "DANGER", List(a1, a2, a3), List(b1, b2))
   }
   it should "work when values are the same" in {
     case class Source(a: Int)
     case class Target(b: Int)
 
-    Mixer[Source, Target].injectq(Target(1), Source(1)) shouldBe Source(1)
+    Mixer[Source, Target].inject(Target(1), Source(1)) shouldBe Source(1)
   }
   it should "not work if target is not wholly contained within source" in {
     case class Source(a: Int)
     case class Target(b: Int, c: String)
 
-    illTyped("Mixer[Source, Target].injectq(Target(1), Source(1)) shouldBe Source(1)")
+    illTyped("Mixer[Source, Target].inject(Target(1), Source(1)) shouldBe Source(1)")
   }
 
 }
